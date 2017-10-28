147fec4383240e711d1572be5cbfd3c2d89e41b5
Stargarth <adamek1221@gmail.com>
2017-10-28 19:07:05 +0200

    Add /status and Latest Digested Post
    This is a stable and tested release.
    When you test with postman be sure that you send datatype JSON

--- src/main/java/api/Api.java
+++ src/main/java/api/Api.java
@@ -1,14 +1,23 @@
 package api;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import datastructures.PostBody;
+import util.StatusMonitor;
 
 @CrossOrigin
 @RestController
 public class Api {
+	
     @Autowired
     public Api() {
         System.out.println("hey");
@@ -17,6 +26,24 @@ public class Api {
     @RequestMapping("/test")
     public String echo(@RequestParam(value = "echo") String echo) {
         return echo;
+    }
+    
+    @RequestMapping(path="/post", method=RequestMethod.POST)
+    public String post(@RequestBody PostBody post) {
+    	
+    		StatusMonitor.setLastPostId(post.getHanesst_id());
+    		return (post.getPost_parent()+" "+ post.getPost_url()+" "+ post.getUsername()+" "+ StatusMonitor.getLastPostId());
+    	//	return "Ok";
+    }
+    
+    @RequestMapping("/status")
+    public String status() {
+    		return StatusMonitor.getStatus();
+    }
+    
+    @RequestMapping("/latest")
+    public int latest() {
+    		return StatusMonitor.getLastPostId();
     }
 
 
---
+++ src/main/java/db/neo4j/Neo4jConnector.java
@@ -0,0 +1,5 @@
+package db.neo4j;
+
+public class Neo4jConnector {
+
+}
