7af594a2bf02f73134024a1cdbb130abe00a07ca
LukaszKoziarski <lkoziars@gmail.com>
2017-11-07 17:16:52 +0100

    Integrated Prometheus to collect data and expose them to the endpoint /metrics

--- src/main/java/api/Api.java
+++ src/main/java/api/Api.java
@@ -1,20 +1,18 @@
 package api;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import datastructures.PostBody;
 import datastructures.User;
 import db.neo4j.MyNeo4jMapper;
+import io.prometheus.client.exporter.MetricsServlet;
+import io.prometheus.client.hotspot.DefaultExports;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.*;
 import util.JSONMapper;
 import util.StatusMonitor;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -34,6 +32,7 @@ public class Api {
 
 	@RequestMapping(path = "/post", method = RequestMethod.POST)
 	public String post(@RequestBody String json) {
+		StatusMonitor.incrementCounter();
 
 		PostBody post = jsonmap.jsonToPostBody(json);
 		mapper.persistPost(post);
@@ -43,29 +42,45 @@ public class Api {
 
 	@RequestMapping(path = "/getPosts", method = RequestMethod.GET)
 	public List<PostBody> getPosts(@RequestParam(value = "limit") int limit) {
-		return mapper.getPostsLimit(limit);
+        StatusMonitor.incrementCounter();
+
+        return mapper.getPostsLimit(limit);
 	}
 	
 	@RequestMapping(path = "/addUser", method = RequestMethod.POST)
 	public boolean addUser(@RequestBody String json) {
+        StatusMonitor.incrementCounter();
+
 		User u = jsonmap.jsonToUser(json);
 		return mapper.addUser(u);
 	}
 	
 	@RequestMapping(path = "/logIn", method = RequestMethod.POST)
 	public boolean logIn(@RequestBody String json) {
+        StatusMonitor.incrementCounter();
+
 		User u = jsonmap.jsonToUser(json);
 		return mapper.logIn(u);
 	}
 
 	@RequestMapping("/status")
 	public String status() {
+        StatusMonitor.incrementCounter();
+
 		return StatusMonitor.getStatus();
 	}
 
 	@RequestMapping("/latest")
 	public int latest() {
+        StatusMonitor.incrementCounter();
+
 		return StatusMonitor.getLastPostId();
 	}
+
+    @Bean
+    ServletRegistrationBean servletRegistrationBean() {
+        DefaultExports.initialize();
+        return new ServletRegistrationBean(new MetricsServlet(), "/metrics");
+    }
 
 }
